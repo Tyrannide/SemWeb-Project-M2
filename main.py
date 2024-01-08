5b68e3f1e93639cb25aa7c9868821ab54ecbe3a4
Alexandre MARINE <alexandre-theo-eliotte@hotmail.com>
2024-01-08 23:28:26 +0100

    Add: fully operational collect.py

--- main.py
+++ main.py
@@ -23,14 +23,17 @@ if __name__ == "__main__":
     app = None
     add = 0
     added = []
+    exist = False
 
     print("Looking for existing data\n")
     if os.path.exists("server_data/data_semweb.db"):
-        g.load("server_data/data_semweb.db")
+        g.parse("server_data/data_semweb.db", format='turtle')
         print("Existing data loaded : \n")
+        exist = True
         #print(g.serialize())
     else:
         print("No existing data found\n")
+        exist = False
 
     sh = "https://schema.org/"
     ex = "http://www.example.com/"
@@ -131,13 +134,82 @@ if __name__ == "__main__":
         else:
             file_path = findfile(file_name, "../../../")
 
-        if file_path[-5:-1] == ".json":
-            with open(file_path, 'r', encoding="UTF-8") as f:
-                data = json.load(f)
-                for city in data:
-                    parseProfessionalServiceJson(city, g)
-        else :
-            print("Given file wasn't from an expected format, please use JSON files\n")
+
+        if  os.path.isfile(file_path):
+            if file_path[-5:] == ".json":
+                with open(file_path, 'r', encoding="UTF-8") as f:
+                    data = json.load(f)
+                    if isinstance(data, list):
+                        to_check = data[0]
+                    else:
+                        to_check = data
+                    if 'coopcycle_url' in to_check:
+                        print("Identified data as coopcycle information\n")
+                        for city in data:
+                            if parseProfessionalServiceJson(city, g) > 0:
+                                add += 1
+                                added.append(city)
+                        else:
+                            print(f"We added {add} element from coopcycle partnership")
+
+                        process = input("Do you want to read the data of all partner registered ?(y/n)\n>>>")
+                        if process in ["y", "Y", "yes", "YES", "Yes", "o", "oui", "Oui", "OUI"]:
+                            for c in added:
+                                urlGroupOfRestaurant = c['coopcycle_url'] + '/api/restaurants'
+                                response = requests.get(urlGroupOfRestaurant)
+                                data = json.loads(response.text)
+                                if 'hydra:member' in data:
+                                    for item in data['hydra:member']:
+                                            urlOfRestaurant = "{0.scheme}://{0.netloc}".format(urlsplit(urlGroupOfRestaurant)) +  item['@id']
+                                            try:
+                                                rResponse = requests.get(urlOfRestaurant)
+                                                rResponse.raise_for_status()
+                                                rData = json.loads(rResponse.text)
+                                                add += parseRestaurantData(rData, g)
+                                            except requests.exceptions.HTTPError as errh:
+                                                print ("HTTP Error:", errh)
+                                            except requests.exceptions.ConnectionError as errc:
+                                                print ("Error Connecting:", errc)
+                                                # Handle connection error
+
+                                            except requests.exceptions.Timeout as errt:
+                                                print ("Timeout Error:", errt)
+                                                # Handle timeout error
+
+                                            except requests.exceptions.RequestException as err:
+                                                print ("Something went wrong:", err)
+                                            
+                                else:
+                                    print(f"No Restaurants found for {c['name']}, request sent :\n{response.text}")
+
+                    else:
+                        print("Considering data as restaurant data\n")
+                        if '@context' in data:
+                            if data['@context'] == '/api/contexts/Menu':
+                                restau = input("Menu context found, from what Restaurant does the menu comes ?\n>>>")
+                                subject_uri = URIRef(ex + restau)
+                                if  (subject_uri, None, None) not in g:
+                                    print(f"Restaurant {restau} was not found, cannot add the menu data to inexistant element")
+                                else:
+                                    menu_node = BNode()
+                                    g.add((subject_uri, URIRef(sh+'hasMenu'), menu_node))
+                                    add += parseMenu(data, g, menu_node)
+                            elif data['@context'] == '/api/contexts/Restaurant':
+                                #print(data)
+                                if 'hydra:member' in data:
+                                    for item in data['hydra:member']:
+                                        urlOfRestaurant = "{0.scheme}://{0.netloc}".format(urlsplit(item['image'])) + item['@id']
+                                        rResponse = requests.get(urlOfRestaurant)
+                                        rData = json.loads(rResponse.text)
+                                        add += parseRestaurantData(rData, g)
+                                else:
+                                    add += parseRestaurantData(data, g)
+                            else:
+                                print("Unknown context, can't process the data")
+            else :
+                print("Given file wasn't from an expected format, please use JSON files\n")
+        else:
+            print("Given input doesnt reach a file\n")
 
     #print(g.serialize())
 
@@ -150,7 +222,7 @@ if __name__ == "__main__":
         r = input("Do you want to save the current data parsed ? (y/n)")
 
         if r in ["y", "Y", "yes", "YES", "Yes", "o", "oui", "Oui", "OUI"]:
-            g.serialize(destination=".server_data/data_semweb.db", format="turtle")
+            g.serialize(destination="server_data/data_semweb.db", format="turtle")
         
         rep = input("Do you want to run the TripleStore ? (y/n)")
 
--- restaurant_checker.py
+++ restaurant_checker.py
@@ -3,8 +3,10 @@ import json
 import requests
 from rdflib import Graph, URIRef, Literal, BNode
 from rdflib.namespace import RDF, XSD, RDFS
+from rdflib.plugins.sparql import prepareQuery
 import pyshacl
 from urllib.parse import urlsplit
+from bs4 import BeautifulSoup
 
 def parseRestaurantData(d, g):
 
@@ -20,42 +22,118 @@ def parseRestaurantData(d, g):
 
     d['name'] = d['name'].replace(" ", "-")
     d['name'] = d['name'].replace('"', "")
+    d['name'] = d['name'].replace('|', "")
 
     print(f"Start of parsing restaurant : {d['name']}")
-
+    subject_uri = URIRef(ex + d['name'])
     # Pre-set up
-    g_to_be_checked.add((URIRef(ex+d['name']), RDF.type, URIRef(sh+"Restaurant")))
-    g_to_be_checked.add((URIRef(ex+d['name']), RDFS.label, Literal(d['description'])))
-    # Process the address
-    address_node = BNode()
-    g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"address"), address_node))
-    g_to_be_checked.add((address_node, RDF.type, URIRef(sh+"Place")))
-    g_to_be_checked.add((address_node, URIRef(sh+"latitude"), Literal(d['address']['geo']['latitude'], datatype=XSD.decimal)))
-    g_to_be_checked.add((address_node, URIRef(sh+"longitude"), Literal(d['address']['geo']['longitude'], datatype=XSD.decimal)))
-    g_to_be_checked.add((address_node, URIRef(sh+"address"), Literal(d['address']['streetAddress'])))
-    if 'tags' in d:     
-        g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"servesCuisine"), Literal(', '.join(d['tags']))))
+    if  (subject_uri, None, None) not in g:
+        g_to_be_checked.add((URIRef(ex+d['name']), RDF.type, URIRef(sh+"Restaurant")))
+        g_to_be_checked.add((URIRef(ex+d['name']), RDFS.label, Literal(d['description'])))
+        g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"nextOpeningDate"), Literal(d['nextOpeningDate'])))
+        # Process the address
+        address_node = BNode()
+        g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"address"), address_node))
+        g_to_be_checked.add((address_node, RDF.type, URIRef(sh+"Place")))
+        g_to_be_checked.add((address_node, URIRef(sh+"latitude"), Literal(d['address']['geo']['latitude'], datatype=XSD.decimal)))
+        g_to_be_checked.add((address_node, URIRef(sh+"longitude"), Literal(d['address']['geo']['longitude'], datatype=XSD.decimal)))
+        g_to_be_checked.add((address_node, URIRef(sh+"address"), Literal(d['address']['streetAddress'])))
+        if 'tags' in d:     
+            g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"servesCuisine"), Literal(', '.join(d['tags']))))
 
-    if 'fulfillmentMethods' in d:
-        for el in d['fulfillmentMethods']:
-            if el['type'] == 'delivery':
-                g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"openingHours"), Literal(', '.join(el['openingHours']))))
+        if 'fulfillmentMethods' in d:
+            for el in d['fulfillmentMethods']:
+                if el['type'] == 'delivery':
+                    g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"openingHours"), Literal(', '.join(el['openingHours']))))
 
-    if 'hasMenu' in d:
-        if 'potentialAction' in d:
-            print("Trying to read Menu")
-            urlOfMenu = "{0.scheme}://{0.netloc}".format(urlsplit(d['potentialAction']['target']['urlTemplate'])) + d['hasMenu']
-            response = requests.get(urlOfMenu)
-            data = json.loads(response.text)
-            menu_node = BNode()
-            g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+'hasMenu'), menu_node))
-            if 'hasMenuSection' in data:
-                for item in data['hasMenuSection']:
-                    for food in item['hasMenuItem']:
-                        added += parseMenu(food, g_to_be_checked, menu_node)
-            else:
-                print(f"No menu found for {d['name']}")
-                
+        if 'hasMenu' in d:
+            if 'potentialAction' in d:
+                print("Trying to read Menu")
+                urlOfMenu = "{0.scheme}://{0.netloc}".format(urlsplit(d['potentialAction']['target']['urlTemplate'])) + d['hasMenu']
+                response = requests.get(urlOfMenu)
+                data = json.loads(response.text)
+                menu_node = BNode()
+                g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+'hasMenu'), menu_node))
+                if 'hasMenuSection' in data:
+                    for item in data['hasMenuSection']:
+                        for food in item['hasMenuItem']:
+                            added += parseMenu(food, g_to_be_checked, menu_node)
+                else:
+                    print(f"No menu found for {d['name']}")
+
+        sorting = list(g.triples((None, URIRef(sh+ "legalName"), None)))
+        for s in sorting:
+            first_level = list(g.triples((s[0], URIRef(sh+ "memberOf"), None)))
+            #print("debugging", first_level)
+            if len(first_level) > 0:
+                for el in first_level:
+                    final_level = list(g.triples((el[2], URIRef(sh+ "url"), None)))
+                    for e in final_level:
+                        check = "{0.scheme}://{0.netloc}".format(urlsplit(d['potentialAction']['target']['urlTemplate']))
+                        #print(e[2], "/", check)
+                        if check == str(e[2]):
+                            membership_node = BNode()
+                            g_to_be_checked.add((URIRef(ex + d['name']), URIRef(sh + "memberOf"), membership_node))
+                            g_to_be_checked.add((membership_node, RDF.type, URIRef(sh + "ProfessionalService")))
+                            #print(el)
+                            g_to_be_checked.add((membership_node, URIRef(sh + "name"), Literal(str(list(g.triples((el[0], URIRef(sh+"legalName"), None)))[0][2]))))
+                            g_to_be_checked.add((membership_node, URIRef(sh + "url"), Literal(str(e[2]), datatype=XSD.anyURI)))
+                    
+    else:
+
+        #print(g_to_be_checked.serialize())
+        print("Data already existing : Updating the old value")
+        g.remove((subject_uri, None, None))
+        g_to_be_checked.add((URIRef(ex+d['name']), RDF.type, URIRef(sh+"Restaurant")))
+        g_to_be_checked.add((URIRef(ex+d['name']), RDFS.label, Literal(d['description'])))
+        g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"nextOpeningDate"), Literal(d['nextOpeningDate'])))
+        # Process the address
+        address_node = BNode()
+        g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"address"), address_node))
+        g_to_be_checked.add((address_node, RDF.type, URIRef(sh+"Place")))
+        g_to_be_checked.add((address_node, URIRef(sh+"latitude"), Literal(d['address']['geo']['latitude'], datatype=XSD.decimal)))
+        g_to_be_checked.add((address_node, URIRef(sh+"longitude"), Literal(d['address']['geo']['longitude'], datatype=XSD.decimal)))
+        g_to_be_checked.add((address_node, URIRef(sh+"address"), Literal(d['address']['streetAddress'])))
+        if 'tags' in d:     
+            g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"servesCuisine"), Literal(', '.join(d['tags']))))
+
+        if 'fulfillmentMethods' in d:
+            for el in d['fulfillmentMethods']:
+                if el['type'] == 'delivery':
+                    g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"openingHours"), Literal(', '.join(el['openingHours']))))
+
+        if 'hasMenu' in d:
+            if 'potentialAction' in d:
+                print("Trying to read Menu")
+                urlOfMenu = "{0.scheme}://{0.netloc}".format(urlsplit(d['potentialAction']['target']['urlTemplate'])) + d['hasMenu']
+                response = requests.get(urlOfMenu)
+                data = json.loads(response.text)
+                menu_node = BNode()
+                g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+'hasMenu'), menu_node))
+                if 'hasMenuSection' in data:
+                    for item in data['hasMenuSection']:
+                        for food in item['hasMenuItem']:
+                            added += parseMenu(food, g_to_be_checked, menu_node)
+                else:
+                    print(f"No menu found for {d['name']}")
+
+        sorting = list(g.triples((None, URIRef(sh+ "legalName"), None)))
+        for s in sorting:
+            first_level = list(g.triples((s[0], URIRef(sh+ "memberOf"), None)))
+            #print("debugging", first_level)
+            if len(first_level) > 0:
+                for el in first_level:
+                    final_level = list(g.triples((el[2], URIRef(sh+ "url"), None)))
+                    for e in final_level:
+                        check = "{0.scheme}://{0.netloc}".format(urlsplit(d['potentialAction']['target']['urlTemplate']))
+                        #print(e[2], "/", check)
+                        if check == str(e[2]):
+                            membership_node = BNode()
+                            g_to_be_checked.add((URIRef(ex + d['name']), URIRef(sh + "memberOf"), membership_node))
+                            g_to_be_checked.add((membership_node, RDF.type, URIRef(sh + "ProfessionalService")))
+                            #print(el)
+                            g_to_be_checked.add((membership_node, URIRef(sh + "name"), Literal(str(list(g.triples((el[0], URIRef(sh+"legalName"), None)))[0][2]))))
+                            g_to_be_checked.add((membership_node, URIRef(sh + "url"), Literal(str(e[2]), datatype=XSD.anyURI)))
 
     if len(g_to_be_checked) != 0:
             #print(g_to_be_checked.serialize())
@@ -68,7 +146,30 @@ def parseRestaurantData(d, g):
 
             else:
                 added += 1
+                
+                list_site = check_through_restaurants(d)
+
+                for site in list_site:
+                    pc, p , etvpc , etvp = scrap(site)
+                    if pc and p and etvpc and etvp:
+                        potentAct = BNode()
+                        priceSpec = BNode()
+                        price_node = BNode()
+                        g_to_be_checked.add((URIRef(ex+d['name']), URIRef(sh+"potentialAction"), potentAct))
+                        g_to_be_checked.add((potentAct, RDF.type, URIRef(sh+"priceSpecification")))
+                        g_to_be_checked.add((potentAct, URIRef(sh+"priceSpecification"), priceSpec))
+                        g_to_be_checked.add((priceSpec, RDF.type, URIRef(sh+"priceSpecification")))
+                        g_to_be_checked.add((priceSpec, URIRef(sh+"priceCurrency"), Literal(pc)))
+                        g_to_be_checked.add((priceSpec, URIRef(sh+"price"), Literal(p)))
+                        g_to_be_checked.add((priceSpec, URIRef(sh+"eligibleTransactionVolume"), price_node))
+                        g_to_be_checked.add((price_node, RDF.type, URIRef(sh+"eligibleTransactionVolume")))
+                        g_to_be_checked.add((price_node, URIRef(sh+"priceCurrency"), Literal(etvpc)))
+                        g_to_be_checked.add((price_node, URIRef(sh+"price"), Literal(etvp)))
+
+
                 g += g_to_be_checked
+                        
+
 
     #if  (subject_uri, None, None) not in g:
 
@@ -95,3 +196,59 @@ def parseMenu(d,g,n):
 
 
     return 1
+
+
+
+
+def check_through_restaurants(d):
+    valid = 0
+    responses = []
+
+    try:
+        response = requests.get(d['potentialAction']['target']['urlTemplate'], timeout = 7)
+        if response.ok:
+            responses.append(response.text)
+            valid +=1
+        else:
+            print(f"Error {response.status_code}: {response.reason}")
+            
+    except requests.exceptions.ReadTimeout:
+        print(f"Site {d['potentialAction']['target']['urlTemplate']} was unresponsive (timed out after 7s)")
+    except requests.exceptions.SSLError:
+        print(f"Site {d['potentialAction']['target']['urlTemplate']} doesn't have a valid certificate, are you sure it is created and configured?")
+    except requests.exceptions.ConnectionError:
+        print(f"Site {d['potentialAction']['target']['urlTemplate']} doesn't exist")
+    except requests.exceptions.TooManyRedirects:
+        print(f"Site {d['potentialAction']['target']['urlTemplate']} had too many redirects, couldn't reach the endpoint")
+    
+    #print(f"{valid}/{total} of urls responded")
+
+    return responses
+
+
+
+def scrap(html_content):
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    script_tag = soup.find('script', {'type': 'application/ld+json'})
+
+    price_specification = None
+
+    if script_tag:
+        json_ld_content = script_tag.string
+        # Parse JSON-LD data
+        try:
+            data = json.loads(json_ld_content)
+
+            price_currency = data.get("potentialAction", {}).get("priceSpecification", {}).get("priceCurrency")
+            price = data.get("potentialAction", {}).get("priceSpecification", {}).get("price")
+            eligible_transaction_volume = data.get("potentialAction", {}).get("priceSpecification", {}).get("eligibleTransactionVolume", {})
+
+
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+    else:
+        print("No script tag with type 'application/ld+json' found.")
+
+    return price_currency, price, eligible_transaction_volume.get("priceCurrency"), eligible_transaction_volume.get("price")
